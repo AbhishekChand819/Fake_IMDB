f0152239ae17abfded8fa20ef8d731568a3434c5
Abhishek Chand <31508692+AbhishekChand819@users.noreply.github.com>
2019-06-19 09:52:41 +0530

    Add files via upload

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { ApiService } from '../api.service';
 import { DataService } from '../data.service';
 import { TvService } from '../datatv.service';
+import { PersonService } from '../person.service';
 
 
 @Component({
@@ -15,10 +16,11 @@ export class NewsComponent implements OnInit {
   value: string;
   datamodal: string;
   mediatype: string;
+  media: boolean;
   result=false;
 
   @ViewChild('movieName',{ read: true, static: false }) movieName: ElementRef;
-  constructor(private apiService: ApiService,private dataService: DataService,private tvService: TvService) { }
+  constructor(private apiService: ApiService,private dataService: DataService,private tvService: TvService,private prService: PersonService) { }
 
   ngOnInit() {
 }
@@ -29,26 +31,61 @@ export class NewsComponent implements OnInit {
     this.apiService.getNews(this.value).subscribe((data)=>{
        console.log(data);
       this.articles = data['results'];
+      this.articles.forEach(function (element){
+        // console.log(element.poster_path);
+        if(element.poster_path == null && element.profile_path==null){
+          element.poster_path = "https://www.rspcansw.org.au/wp-content/themes/noPhotoFound.png"; 
+          element.profile_path = "";
+        } else if(element.profile_path == null) {
+          element.poster_path = "https://image.tmdb.org/t/p/w600_and_h900_bestv2/"+element.poster_path;
+          element.profile_path="";
+        } else if(element.poster_path == null) {
+          element.profile_path = "https://image.tmdb.org/t/p/w600_and_h900_bestv2/"+element.profile_path;
+          element.poster_path="";
+        }
+      });
     });
   }
 
   getData(datamodal,mediatype){
     if(mediatype=='tv')
     {
+      this.media=true;
       this.tvService.getData(datamodal).subscribe((data)=>{
-         console.log(data);
         this.modals = data;
+        if(this.modals.poster_path == null){
+          this.modals.poster_path = "https://www.iol.co.za/assets/images/general/no-image.png"; 
+        } else {
+          this.modals.poster_path = "https://image.tmdb.org/t/p/w600_and_h900_bestv2/"+this.modals.poster_path;
+        }
       });
     }
     else if(mediatype=='movie')
     {
       // console.log(datamodal);
+      this.media=true;
       this.dataService.getData(datamodal).subscribe((data)=>{
-       console.log(data);
       this.modals = data;
+      if(this.modals.poster_path == null){
+        this.modals.poster_path = "https://www.iol.co.za/assets/images/general/no-image.png"; 
+      } else {
+        this.modals.poster_path = "https://image.tmdb.org/t/p/w600_and_h900_bestv2/"+this.modals.poster_path;
+      }
     });
     }
-
+    else if(mediatype=='person')
+    {
+      this.media=false;
+      this.prService.getData(datamodal).subscribe((data)=>{
+      this.modals = data;
+      // console.log(this.modals);
+      if(this.modals.profile_path == null){
+        this.modals.poster_path = "https://www.iol.co.za/assets/images/general/no-image.png"; 
+      } else {
+        this.modals.poster_path = "https://image.tmdb.org/t/p/w600_and_h900_bestv2/"+this.modals.profile_path;
+      }
+    });
+    }
 
   }
 }
